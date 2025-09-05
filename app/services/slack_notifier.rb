a9c58376eee6f2e53536a5d4d0dbafb3e2cd2c1e
Payal Sharma <payal.sharma@kaleris.com>
2025-09-05 17:00:01 +0530

    Add URL

--- app/services/slack_notifier.rb
+++ app/services/slack_notifier.rb
@@ -1,9 +1,7 @@
 # app/services/slack_notifier.rb
 class SlackNotifier
   def self.notify(message, channel: "@CI-CD Notifier")
-    notifier = Slack::Notifier.new("https://hooks.slack.com/services/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")
+    notifier = Slack::Notifier.new("https://hooks.slack.com/services/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")
     notifier.post(text: message, channel: channel)
   end
 end
-
-# curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}' https://hooks.slack.com/services/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
